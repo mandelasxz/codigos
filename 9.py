7668f13de32d40e54ec71a032fa2f9d19a17e673
mandelasxz <81700392+mandelasxz@users.noreply.github.com>
2023-02-13 12:02:02 -0300

    janela

---
+++ 9.py
@@ -0,0 +1,34 @@
+
+""""
+
+from tkinter.ttk import *
+from tkinter import*
+
+
+
+co0 = "2e2d2b"
+co0 = 
+
+"2e2d2b"
+co0 = "2e2d2b"
+co0 = "2e2d2b"
+co0 = "2e2d2b"
+co0 = "2e2d2b"
+co0 = "2e2d2b"
+co0 = "2e2d2b"
+janela = Tk()
+janela.title ("")
+janela.geometry('410x400')
+janela.configure(background=co0)
+
+
+frameCima = Frame(janela, width=418, height=200, bg=co0, relief="flat",)
+
+
+
+"""
+
+
+
+
+
